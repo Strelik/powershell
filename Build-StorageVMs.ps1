481cfc8cc132bdb27a4cf47adfe6387ee1cad28f
unknown <jason@jrdoppes.com>
2015-09-17 23:32:45 -0400

    Added Initial Build-StorageVMs script

---
+++ Build-StorageVMs.ps1
@@ -0,0 +1,62 @@
+﻿
+
+#Script to create a handful of storage-oriented VirtualBox VMS automatically with multiple hard-drives attached to form a cluster.
+#IE Ceph, Replication Nodes, Shared Storage ETC
+
+
+
+function Build-VMs ()
+{
+    [CMDletBinding()]
+    Param(
+       
+      [Parameter(Mandatory=$True,Position=1)]
+      [String]$ostype,
+
+      [Parameter(Mandatory=$True)]
+      [int]$hosts
+    )
+
+    if ($hosts -lt 1)
+    {
+       echo "Please enter a number of hosts greater than 0"
+       exit
+    }
+    
+    $counter = 0
+
+    DO{
+    
+    
+    VBoxManage createvm --name "StorageHost_$counter"  --register
+    VBoxManage modifyvm "StorageHost_$counter" --ostype $ostype
+
+    
+    VBoxManage modifyvm "StorageHost_$counter" --memory 4096
+    VBoxManage modifyvm "StorageHost_$counter" --cpus 2
+    VBoxManage modifyvm "StorageHost_$counter" --ioapic on
+
+    VBoxManage storagectl "StorageHost_$counter" --name IDE --add ide --controller PIIX4 --bootable on
+    VBoxManage storagectl "StorageHost_$counter" --name SATA --add sata --controller IntelAhci --bootable on
+    VBoxManage storagectl "StorageHost_$counter" --name SAS --add sas --controller LSILogicSAS
+    
+    VBoxManage createhd --filename "Boot_StorageHost_$counter.vdi" --size 32768
+    VBoxManage createhd --filename "SAS_StorageHost_$counter.vdi" --size 131072
+     
+
+    VBoxManage storageattach "StorageHost_$counter" --storagectl IDE --port 0 --device 0 --type dvddrive --medium emptydrive
+    VBoxManage storageattach "StorageHost_$counter" --storagectl SATA --port 0 --device 0 --type hdd --medium Boot_StorageHost_$counter.vdi
+    VBoxManage storageattach "StorageHost_$counter" --storagectl SAS --port 0 --device 0 --type hdd --medium SAS_StorageHost_$counter.vdi
+
+    VBoxManage modifyvm "StorageHost_$counter" --nic1 nat --nictype1 82540EM --cableconnected1 on
+    VBoxManage modifyvm "StorageHost_$counter" --nic2 bridged --nictype1 82540EM --cableconnected1 on
+
+    
+    $counter++
+    
+    } While ($counter -le ($hosts-1))
+       
+
+}
+
+Build-VMs
